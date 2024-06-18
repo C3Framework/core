e344c617dcbe47640e86db34328883be646dcf80
Luan Himmlisch <luan@himmlisch.com.mx>
2024-06-17 22:31:48 -0600

    Commented build config

--- types/config.ts
+++ types/config.ts
@@ -32,17 +32,69 @@ export interface BuiltAddonConfig extends AddonConfig {
 };
 
 export interface BuildConfig {
+    /** 
+     * Safely minify the code 
+     * @default true
+     */
     minify?: boolean,
+    /**
+     * The host domain of the server
+     * @default 'http://localhost'
+     */
     host?: string,
+    /**
+     * The port of the server
+     * @default 3000
+     */
     port?: number,
+    /**
+     * The path of the main code, such as the addon config, runtime & editor script.
+     * @default 'src/'
+     */
     sourcePath?: string,
-    addonScript?: string,
-    defaultLang?: string,
+    /**
+     * The entry point for compilation. It should be the script that is run on runtime
+     * @default 'runtime.ts'
+     */
     runtimeScript?: string,
-    langPath?: string,
-    libPath?: string,
-    defPath?: string,
+    /**
+     * The name of the script where the addon config will be exported
+     * @default 'addon.ts'
+     */
+    addonScript?: string,
+    /**
+     * The path of the parsed & compiled assets, it will get deleted and recreated on each build
+     * @default 'export/'
+     */
     exportPath?: string,
-    examplesPath?: string,
+    /**
+     * The path where the distributed `.c3addon` will get saved to
+     * @default 'dist/'
+     */
     distPath?: string,
+    /**
+     * The path that will get scanned for dependencies to ship
+     * @default 'src/libs'
+     */
+    libPath?: string,
+    /**
+     * The path of the language dictionaries that will get loaded on compilation
+     * @default 'src/lang'
+     */
+    langPath?: string,
+    /**
+     * The path that will get scanned for TypeScript definition files `.d.ts` to ship
+     * @default 'src/'
+     */
+    defPath?: string,
+    /**
+     * The path of the `.c3p` examples that will get shipped
+     * @default 'examples/'
+     */
+    examplesPath?: string,
+    /**
+     * The language that will get shipped by default and that the translated strings will fallback
+     * @default 'en-US'
+     */
+    defaultLang?: string,
 }
